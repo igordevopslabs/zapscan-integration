b2efad528dcefce8802c294dcd4eb3d5efb4c8c5
igoritos22 <igoritos_22@hotmail.com>
2024-06-01 11:00:42 -0300

    feat: add swaggear

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/igordevopslabs/zapscan-integration/config"
+	docs "github.com/igordevopslabs/zapscan-integration/docs"
 	"github.com/igordevopslabs/zapscan-integration/migrations"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -12,7 +15,20 @@ func init() {
 	migrations.SyncDatabase()
 }
 
+// @title API ZapScan Integration
+// @version 1.0
+// @description A simple REST API to integration a ZAProxy vulnerability scans
+// @host localhost:3000
+// @BasePath /
+// @SecurityDefinitions BasicAuth
+// @in header
+// @name Authorization
 func main() {
 	r := gin.Default()
+
+	//Documentation
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 	r.Run()
 }
