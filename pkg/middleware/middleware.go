68304cafb66a90cfa94bc8eea15889f63945ad0e
igoritos22 <igoritos_22@hotmail.com>
2024-06-09 11:44:01 -0300

    feat: remove hardcode

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -2,14 +2,25 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/igordevopslabs/zapscan-integration/config"
 )
+
+var basicUser string
+var basicPass string
+
+func init() {
+	config.LoadEnvs()
+	basicUser = os.Getenv("BASIC_USER")
+	basicPass = os.Getenv("BASIC_PASS")
+}
 
 func BasicAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		user, pass, hashAuth := c.Request.BasicAuth()
-		if hashAuth && user == "admin" && pass == "password" {
+		if hashAuth && user == basicUser && pass == basicPass {
 			c.Next()
 			return
 		}
